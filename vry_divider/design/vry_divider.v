097673f1d8bf1023cfe831806c04b92a60723ddd
hanfei <hanfei@byhx-china.com>
2020-07-08 09:25:39 +0800

    create vry_divider 202007080925

---
+++ vry_divider/design/vry_divider.v
@@ -0,0 +1,19 @@
+module	vry_divider(
+		input		wire		clk,
+		input		wire		rst_n
+);
+
+
+
+
+
+divider_1 divider_1_inst(
+	.clock		(),
+	.denom		(),
+	.numer		(),
+	.quotient	(),
+	.remain		()
+	)
+
+
+endmodule
